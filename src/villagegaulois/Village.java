d9b8d6973b05386eceaf7362e2050715ed19a352
MKZenit <enzodanige@gmail.com>
2024-02-28 11:13:13 +0100

    Village -> Classe interne Marche

--- src/villagegaulois/Village.java
+++ src/villagegaulois/Village.java
@@ -1,5 +1,5 @@
 package villagegaulois;
-
+import java.util.Arrays;
 import personnages.Chef;
 import personnages.Gaulois;
 
@@ -8,10 +8,67 @@ public class Village {
 	private Chef chef;
 	private Gaulois[] villageois;
 	private int nbVillageois = 0;
+	private Marche marche;
 
-	public Village(String nom, int nbVillageoisMaximum) {
+	public Village(String nom, int nbVillageoisMaximum, int nbEtals) {
 		this.nom = nom;
 		villageois = new Gaulois[nbVillageoisMaximum];
+		this.marche = new Marche(nbEtals);
+	}
+	
+	private static class Marche{
+		private Etal[] etals;
+		
+		public Marche(int nbEtal) {
+			etals = new Etal[nbEtal];
+		}
+		
+		public void utiliserEtal(int indiceEtal, Gaulois vendeur, String produit, int nbProduit) {
+			this.etals[indiceEtal].occuperEtal(vendeur, produit, nbProduit);
+		}
+		
+		public int trouverEtalLibre() {
+			for(int i=0; i<etals.length; i++) {
+				if(!etals[i].isEtalOccupe()) return i;
+			}
+			return -1;
+		}
+		
+		public Etal[] trouverEtals(String produit) {
+			int nbEtalsTrouves = 0;
+			for(int i=0; i<this.etals.length; i++) 
+				if(this.etals[i].contientProduit(produit))
+					nbEtalsTrouves += 1;
+			
+			Etal[] etalsTrouves = new Etal[nbEtalsTrouves];
+			int iEtalTrouve = 0;
+			for(int i=0; i<this.etals.length; i++)
+				if(this.etals[i].contientProduit(produit)) {
+					etalsTrouves[iEtalTrouve] = this.etals[i];
+					iEtalTrouve += 1;
+				}
+		
+			return etalsTrouves;
+		}
+		
+		public Etal trouverVendeur(Gaulois gaulois) {
+			for(int i=0; i<this.etals.length; i++) {
+				if(this.etals[i].getVendeur()==gaulois) {
+					return this.etals[i];
+				}
+			}
+			return null;
+		}
+		
+		public String afficherMarche() {
+			String result = "";
+			int i;
+			for(i=0; i<this.etals.length && this.etals[i]!=null; i++)
+				result += this.etals[i].afficherEtal();
+			
+			return result + "Il reste " + (this.etals.length-i) + " étals non utilisés dans le marché.\n";
+		}
+		
 	}
 
 	public String getNom() {
